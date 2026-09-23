ed5af90cfab124cbac3faad2d1b01d6a212fcac7
gdsstsystrstrsx <reddyln123@gmail.com>
2026-09-23 11:28:23 +0530

    new

--- SeleniumMaven/src/test/java/pac1/contactTest.java
+++ SeleniumMaven/src/test/java/pac1/contactTest.java
@@ -22,8 +22,5 @@ public class contactTest {
 	public void modifycontactTest() {
 		System.out.println("Execute modifycontacttest");
 	}
-	@Test
-	public void DeletecontactTest() {
-		System.out.println("Execute DeletecontactTest");
-	}
+	
 }
--- SeleniumMaven/src/test/java/pac2/orgTest.java
+++ SeleniumMaven/src/test/java/pac2/orgTest.java
@@ -7,6 +7,15 @@ public class orgTest {
 	@Test
 	public void createorgTest() {
 		System.out.println("Execute createorgtest");
+
+		String URL =System.getProperty("url");
+		String BROWSER =System.getProperty("browser");
+		String USERNAME =System.getProperty("username");
+		String PASSWORD =System.getProperty("password");
+		System.out.println(URL);
+		System.out.println(BROWSER);
+		System.out.println(USERNAME);
+		System.out.println(PASSWORD);
 	}
 	
 	@Test

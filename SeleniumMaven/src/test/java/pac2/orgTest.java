61588a2cbe058eed22ed37a4c1ecbd8d9e7579f6
gdsstsystrstrsx <reddyln123@gmail.com>
2026-09-23 10:19:55 +0530

    hiii

---
+++ SeleniumMaven/src/test/java/pac2/orgTest.java
@@ -0,0 +1,16 @@
+package pac2;
+
+import org.testng.annotations.Test;
+
+public class orgTest {
+
+	@Test
+	public void createorgTest() {
+		System.out.println("Execute createorgtest");
+	}
+	
+	@Test
+	public void modifyorgTest() {
+		System.out.println("Execute modifyorgtest");
+	}
+}
